38bb3b3d9d187a3b47218c9a0c66aae9182e9e82
Nicolas Santana <Nicolas Santana@NICOLAS>
2018-03-14 14:56:53 -0600

    cruza

---
+++ CruzaP/CruzaP/Individo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CruzaP
+{
+    class Individo
+    {
+        Individo [] Cross1P(int [] f1,int [] m1,int crosspoint)
+        {
+            Individo[] hijo = new Individo[2];
+
+            for (int i = 0; i < m1.Length ; i ++)
+            {
+                if (i <= crosspoint)
+                {
+                    hijo[0] = f1[i];
+
+                }
+                else
+                {
+                    hijo[0] = m1[i+1];
+                }
+
+            }
+            
+
+
+        }
+    }
+}
